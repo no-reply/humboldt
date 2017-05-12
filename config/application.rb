efa19de02e8ad32d8b905a469f7a61f794b8cf8f
Tom Johnson <tom@dp.la>
2017-05-12 09:18:27 -0700

    Generate hyrax app

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,17 @@ Bundler.require(*Rails.groups)
 
 module Humboldt
   class Application < Rails::Application
+    
+
+      # The compile method (default in tinymce-rails 4.5.2) doesn't work when also
+      # using tinymce-rails-imageupload, so revert to the :copy method
+      # https://github.com/spohlenz/tinymce-rails/issues/183
+      config.tinymce.install = :copy
+    config.generators do |g|
+      g.test_framework :rspec, :spec => true
+    end
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
